422cd867baf21bd87ad775ffcc33e9f6c285cfab
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2020-04-21 14:19:48 -0300

    chore: install styleguide dependencies

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 module.exports = {
   // page entrypoints must end with those prefixes (i.e.: "index.page.tsx", "api.page.ts"):
   pageExtensions: ["page.tsx", "page.ts"],
   typescript: { ignoreDevErrors: true, ignoreBuildErrors: true },
-  webpack: config => {
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.(graphql|gql)$/u,
       exclude: /node_modules/u,
